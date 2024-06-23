a046e503fe1fa78fe07f67f775aabdc8c2dda44e
b3rg3t <berget_90db@hotmail.com>
2024-06-23 22:34:37 +0200

    fix build error

--- src/__mocks__/data/RoundsMock.ts
+++ src/__mocks__/data/RoundsMock.ts
@@ -6,11 +6,6 @@ import { getDefaultScore } from "../../store/reducers/game/helper";
 export const roundsMock: TRound[] = [
   {
     roundId: nanoid(),
-    players: [
-      playersMock[0].playerId,
-      playersMock[1].playerId,
-      playersMock[2].playerId,
-    ],
     round: 1,
     created: new Date().toLocaleString(),
     score: getDefaultScore(playersMock.map((player) => (player.playerId)))
