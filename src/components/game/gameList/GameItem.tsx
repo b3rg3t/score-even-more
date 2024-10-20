a0e7a8f5a25c59c02dde3e3e454b3d229b45452f
b3rg3t <berget_90db@hotmail.com>
2024-10-20 17:46:21 +0200

    update layout of gameList

--- src/components/game/gameList/GameItem.tsx
+++ src/components/game/gameList/GameItem.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 import { useAppDispatch, useAppSelector } from "../../../store/redux/hooks";
-import { selectByGameId, setActiveGame } from "../../../store/reducers/game/gameSlice";
+import {
+  selectByGameId,
+  setActiveGame,
+} from "../../../store/reducers/game/gameSlice";
 import { IGame } from "../../../models/interface/IGame";
+import { FaUsers } from "react-icons/fa6";
 
 export interface IGameItem {
   gameId: IGame["gameId"];
@@ -15,14 +19,31 @@ export const GameItem: FC<IGameItem> = ({ gameId, callBackFunction }) => {
   const handleSetActiveGame = () => {
     if (game) {
       dispatch(setActiveGame(game));
-      callBackFunction && callBackFunction()
+      callBackFunction && callBackFunction();
     }
   };
 
   return (
     <li id={gameId}>
-      <button className="btn text-white border w-100" type="button" onClick={handleSetActiveGame}>
-        {game?.gameSettings?.gameName}
+      <button
+        className="btn btn-outline-primary text-white w-100"
+        type="button"
+        onClick={handleSetActiveGame}
+      >
+        <div className="d-flex justify-content-between w-100">
+          <div className="d-flex gap-2">
+            <span className="d-flex align-items-center badge rounded-pill text-bg-info text-white">
+              {game?.rounds.length}
+            </span>
+            <span>{game?.gameSettings?.gameName}</span>
+          </div>
+          <div className="d-flex gap-2">
+            <span className="d-flex gap-1 align-items-center">
+              <FaUsers />
+              {game?.playerIds.length}
+            </span>
+          </div>
+        </div>
       </button>
     </li>
   );
