2419e3b0d5c9b0deb44e5d1a67e87257ec60eb98
b3rg3t <berget_90db@hotmail.com>
2025-02-18 22:16:08 +0100

    add minor padding for android on footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -94,7 +94,7 @@ export const Footer = () => {
         modalHeight={500}
         {...renderModalContent()}
       />
-      <div className={`footer bg-dark sticky-bottom border-top shadow text-white p-1 d-flex justify-content-around ${isMobileSafari || isSafari ? "pb-4" : ""}`}>
+      <div className={`footer bg-dark sticky-bottom border-top shadow text-white p-1 d-flex justify-content-around ${isMobileSafari || isSafari ? "pb-4" : "pb-2"}`}>
         <FooterButton
           modalType="createGame"
           text={createGame}
