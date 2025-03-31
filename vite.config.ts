70abce84d17d0d278f6cfade8f9b385826ee37fd
b3rg3t <berget_90db@hotmail.com>
2025-03-31 19:23:27 +0200

    test with new icons

--- vite.config.ts
+++ vite.config.ts
@@ -11,39 +11,30 @@ const manifestConfig: Partial<VitePWAOptions> = {
     description:
       "Keep your score in this immersive scoreboard for all your games",
     icons: [
+      { src: "icons/16x16.png", sizes: "16x16", type: "image/png" },
+      { src: "icons/32x32.png", sizes: "32x32", type: "image/png" },
+      { src: "icons/72x72.png", sizes: "72x72", type: "image/png" },
+      { src: "icons/96x96.png", sizes: "96x96", type: "image/png" },
+      { src: "icons/120x120.png", sizes: "120x120", type: "image/png" },
+      { src: "icons/128x128.png", sizes: "128x128", type: "image/png" },
+      { src: "icons/144x144.png", sizes: "144x144", type: "image/png" },
+      { src: "icons/152x152.png", sizes: "152x152", type: "image/png" },
+      { src: "icons/180x180.png", sizes: "180x180", type: "image/png" },
+      { src: "icons/192x192.png", sizes: "192x192", type: "image/png" },
+      { src: "icons/384x384.png", sizes: "384x384", type: "image/png" },
+      { src: "icons/512x512.png", sizes: "512x512", type: "image/png" },
       {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-512x512.png",
+        src: "icons/512x512.png",
         sizes: "512x512",
         type: "image/png",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-      {
-        src: "/maskable_icon.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-512x512.svg",
-        sizes: "512x512",
-        type: "image/svg",
+        purpose: "maskable",
       },
     ],
     theme_color: "#0a0127",
     background_color: "#0a0127",
     display: "standalone",
-    scope:
-      "https://score-even-more.vercel.app/",
-    start_url:
-      "https://score-even-more.vercel.app/",
+    scope: "https://score-even-more.vercel.app/",
+    start_url: "https://score-even-more.vercel.app/",
     orientation: "portrait",
   },
   devOptions: {
